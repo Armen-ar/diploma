65972f122b24d45a27a167d2c11f3479912a4b3a
Armen-ar <armen.aruts1967@gmail.com>
2022-11-27 21:15:46 +0300

    add login endpoint

--- core/urls.py
+++ core/urls.py
@@ -4,7 +4,7 @@ from core import views
 
 urlpatterns = [
     path('signup', views.RegistrationView.as_view(), name='signup'),
+    path('login', views.LoginView.as_view(), name='login'),
     # path('profile', views.______View.as_view(), name='profile'),
-    # path('login', views..______View.as_view(), name='login'),
     # path('update_password', views..______View.as_view(), name='update_password'),
 ]
