a314db65002120629423a0658753a3512b6abdfe
Armen-ar <armen.aruts1967@gmail.com>
2022-11-27 21:40:43 +0300

    add retrieve, update profile

--- core/urls.py
+++ core/urls.py
@@ -5,6 +5,6 @@ from core import views
 urlpatterns = [
     path('signup', views.RegistrationView.as_view(), name='signup'),
     path('login', views.LoginView.as_view(), name='login'),
-    # path('profile', views.______View.as_view(), name='profile'),
+    path('profile', views.ProfileView.as_view(), name='profile'),
     # path('update_password', views..______View.as_view(), name='update_password'),
 ]
