fa2368c08629157a2232732defb5840fdeba1e4c
Armen-ar <armen.aruts1967@gmail.com>
2022-12-18 17:01:38 +0300

    add bot

---
+++ bot/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+
+class TgUser(models.Model):
+    chat_id = models.BigIntegerField(verbose_name='Номер чата.')
+    user_ud = models.BigIntegerField(
+        verbose_name='Пользовательский идентификатор.'
+    )
+    user = models.ForeignKey(
+        'core.User', 
+        on_delete=models.PROTECT, 
+        null=True, blank=True, 
+        verbose_name='Пользователь.'
+    )
+    verification_code = models.CharField(
+        max_length=255, 
+        null=True, 
+        blank=True, 
+        verbose_name='Код верификации.'
+    )
---
+++ bot/tg/__init__.py
@@ -0,0 +1,13 @@
+import os
+
+from .client import TgClient
+from .dc import get_updates_schema, send_message_schema
+
+tg_client = TgClient(os.environ.get('TG_TOKEN'))
+
+__all__ = (
+    'get_updates_schema',
+    'send_message_schema',
+    'tg_client',
+    'TgClient'
+)
